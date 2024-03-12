0a049a47797a966462908b2f0921c239bdd4863a
DiegoClavijo300 <munnirclavijo@gmail.com>
2024-03-11 21:21:03 -0500

    finished workshop

---
+++ testing.js
@@ -0,0 +1,39 @@
+// Unit Test
+
+// Multiplication function
+// expect product of multiplication to be a number 
+// expext product of (2,3) to be equal to 6
+// expect product of ("a",1) to be error
+// expect to multiply all numbers inserted
+// expect the porduct of any number * 0 to be equal to 0
+// expect the product of (-1,3) to be equal to -3
+
+//contactOdds
+// expect the result of contactOdds to be a number
+// expect the input of (strings or object) to be ignored
+// expect the entry of the same number multiple times to result in the number being printed only once
+// expect the function to give a result of only odd numbers
+// expect the result of [1,2,3,4,5][6,7,8,9] to be equal to [1,3,5,7,9]
+// expect the result to be ONE array of numbers
+// expect the result of contactOdds to return an array of numbers in ascending order
+
+// functional Test 
+
+// When user clicks on shopping cart a list of the added items should appear 
+// if the cart is empty an option to return to shopping should appear
+// when user scrolls down on the shopping cart there should be a button for checkout 
+// when user clicks on checkout a prompt should appear the requires "login to checkout", or "checkout as guest"
+// if user is already logged in to their account the prompt should not pop in 
+// when user selects "log in to checkout", a new page should appear to authentucate the credentials for log in (user name and password)
+// when user selects "checkout as a guest" a new page appears with all the items of the shopping cart and a total for the ammount to pay
+// when in the check out as a guest page user should have an option to create and account with details of why it is more convenient to have an account for future purchases 
+// when  in the checkout as a guest page user should have an option to log in 
+// when user has reviewed the items and is ready to pay they can click on pay now 
+// when user clicks on pay now they should be taken to a new window for authentication of payment
+// when user is in new window there should be options for payments (credit card, debit card)
+//when user is in the payment window user will need to fill our payment info and billing info (name, address, email, phone number)
+// when the user takes more than 10 minutes in the payment window, the user will be taken back to the cart window to restart process 
+// when authentication process fails, allow the user to re-enter their payment information
+// when payment fails 3 times in a row, take user back to cart window
+// when payment is approved, the user should receive an email with confirmation of  order and details of the order 
+// when payment is approved user sshould be taken to a new window with confirmation of purchase and details of the trnsaction 
